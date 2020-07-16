30576eb69c7565b1022cf197e455b55ca3d1adae
Pawel Grabczak <grabczak.pa@gmail.com>
2020-07-16 18:59:51 +0200

    add models

--- store_web/models.py
+++ store_web/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import PermissionsMixin, AbstractUser
+from django.contrib.auth.models import PermissionsMixin
 from django.core.mail import send_mail
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
