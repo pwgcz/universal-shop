ccea0962ed68abc2d796fc87fb40ef0011bafee0
Pawel Grabczak <grabczak.pa@gmail.com>
2020-07-31 10:26:33 +0200

    add requirements-dev

--- front_end/src/pages/Products.js
+++ front_end/src/pages/Products.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+
 
 const Products = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(()=>{
+    axios.get(`/api/products`)
+    .then((res)=>{
+      setProducts(res.data.results)
+    })
+  }, []);
+
+  console.log({products});
   return (
     <div>
-    Product side
+    product
     </div>
   )
 }
