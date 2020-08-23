2fe7f6aad3b382ed469f986712f115d14be64b5a
Pawel Grabczak <grabczak.pa@gmail.com>
2020-08-23 12:30:15 +0200

    add erro page

--- front_end/src/pages/Error.js
+++ front_end/src/pages/Error.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom'
+
 
 const Error = () => {
   return(
-    <div>
-    Error page
+    <div className='contentWrapper'>
+      <Banner title='404' subtitle='page not found'>
+        <Link to='/' className='btn-primary'>
+          return 
+        </Link>
+      </Banner>
     </div>
   )
 }
