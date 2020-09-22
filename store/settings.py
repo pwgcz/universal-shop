c9c6fcfb38f2507e80bebcd0cb214e0af672a81c
Pawel Grabczak <grabczak.pa@gmail.com>
2020-09-22 14:29:10 +0200

    heroku deploy

--- store/settings.py
+++ store/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=True, cast=bool)
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 INSTALLED_APPS = [
